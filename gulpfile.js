25f2d6cdcaf9ffa1ff3cfd3583f4420cdd88d7a7
Jesus Gonzalez <ivppiter@gmail.com>
2016-10-16 12:12:04 -0500

    configure tests environment

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,32 @@ var ngAnnotate    = require('browserify-ngannotate');
 var browserSync   = require('browser-sync').create();
 var concat        = require('gulp-concat');
 
-function interceptErrors (error) {
-  var args = Array.prototype.slice.call(arguments);
+function setupInterceptErrorsFor(title){
+  return function (error) {
+    var args = Array.prototype.slice.call(arguments);
 
-  notify.onError({
-    title: 'Compile Error',
-    message: '<%= error.message %>'
-  }).apply(this, args);
+    notify.onError({
+      title: title,
+      message: '<%= error.message %>'
+    }).apply(this, args);
 
-  this.emit('end');
-};
+    this.emit('end');
+  }
+}
 
 gulp.task('browserify', function() {
   return browserify('./src/app.js')
       .transform(babelify, {presets: ['es2015']})
       .transform(ngAnnotate)
       .bundle()
-      .on('error', interceptErrors)
+      .on('error', setupInterceptErrorsFor('Compile error'))
       .pipe(source('index.js'))
       .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('html', function() {
   return gulp.src('./index.html')
-      .on('error', interceptErrors)
+      .on('error', setupInterceptErrorsFor('Compile error'))
       .pipe(gulp.dest('./build/'));
 });
 
@@ -46,6 +48,18 @@ gulp.task('fonts', function(){
   return gulp
     .src(['node_modules/bootstrap/dist/fonts/**/*'])
     .pipe(gulp.dest('./build/fonts'))
+})
+
+var karma = require('gulp-karma');
+
+gulp.task('test', ['html', 'browserify', 'styles', 'fonts'], function(){
+  return gulp
+    .src('./dummy-file')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', setupInterceptErrorsFor('Test error'))
 })
 
 gulp.task('default', ['html', 'browserify', 'styles', 'fonts'], function() {
